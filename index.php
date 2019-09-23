fcdb40b5f08fd489361f0ceb73e0b7e2a4e66952
Rama <mramamaulana60@gmail.com>
2019-09-23 13:40:17 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php include 'header.php'; ?>
 	<div class="uk-text-center">
-		
+		<h1>M. Rama Maulana</h1>
+		<h4>Beriku adalah hasil kerja saya dalam satu minggu ini.</h4>
+		<div class="uk-flex uk-flex-center">
+			<a href="v1.php"><img src="media/v1.png" width="500"></a>
+			<a href="v2.php"><img src="media/v2.png" width="500"></a>
+		</div>
 	</div>
-<?php include 'foooter.php'; ?>
+<?php include 'footer.php'; ?>
